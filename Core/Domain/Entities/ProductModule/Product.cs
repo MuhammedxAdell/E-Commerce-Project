07dbfc1c4b28e529d07b8b17098c15f188005e22
Mohamed Adel <111653519+MuhammedxAdell@users.noreply.github.com>
2025-10-14 21:58:27 +0300

    Product & ProductBrand & ProductType Entities

---
+++ Core/Domain/Entities/ProductModule/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities.ProductModule
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string PictureUrl { get; set; } = string.Empty;
+
+        //1-M Product-ProductType
+        public ProductType ProductType { get; set; } // Navigation Property
+        public int TypeId { get; set; } // Foreign Key
+
+        //1-M Product-ProductBrand
+        public ProductBrand ProductBrand { get; set; } // Navigation Property
+        public int BrandId { get; set; } // Foreign Key
+    }
+}
---
+++ Core/Domain/Entities/ProductModule/ProductBrand.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities.ProductModule
+{
+    public class ProductBrand : BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
