b0c1ca20eb6d6fdd14f6fc9864acc6329e9b3a1e
Mohamed Adel <111653519+MuhammedxAdell@users.noreply.github.com>
2025-10-29 04:35:55 +0300

    Authentication Service [Login & Register ]

---
+++ Core/Services.Abstraction/Contracts/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Dtos.IdentityModule;
+
+namespace Services.Abstraction.Contracts
+{
+    public interface IAuthenticationService
+    {
+        //Login     ==> return UserResultDto [ DisplayName , Token , Email ] ==> Take Params [Email , Password ]
+        Task<UserResultDto> LoginAsync(LoginDto loginDto);
+        //Register  ==> return UserResultDto [ DisplayName , Token , Email ] ==> Take Params [ DisplayName , Email , Password , PhoneNumber , UserName ]
+        Task<UserResultDto> RegisterAsync(RegisterDto registerDto);
+
+    }
+}
---
+++ Core/Services/Implementations/AuthenticationService.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities.IdentityModule;
+using Domain.Exceptions;
+using Microsoft.AspNetCore.Identity;
+using Services.Abstraction.Contracts;
+using Shared.Dtos.IdentityModule;
+using System.ComponentModel.DataAnnotations;
+using ValidationException = Domain.Exceptions.ValidationException;
+
+namespace Services.Implementations
+{
+    public class AuthenticationService(UserManager<User> _userManager) : IAuthenticationService
+    {
+        public async Task<UserResultDto> LoginAsync(LoginDto loginDto)
+        {
+            //Email exists ?
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user is null) throw new UnauthorizedException();
+            //Password is correct ?
+            var isPasswordValid = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+            if (!isPasswordValid) throw new UnauthorizedException();
+            return new UserResultDto(user.DisplayName, "Token", user.Email);
+        }
+            
+
+        public async Task<UserResultDto> RegisterAsync(RegisterDto registerDto)
+        {
+            var user = new User
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.UserName,
+                PhoneNumber = registerDto.PhoneNumber
+            };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(e => e.Description);
+                throw new ValidationException(errors);
+            }
+            return new UserResultDto(user.DisplayName, "Token", user.Email);
+        }
+    }
+}
