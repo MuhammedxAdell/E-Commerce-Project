3985797bea221fead6a4d2ecad6a79f9efd75d38
Mohamed Adel <111653519+MuhammedxAdell@users.noreply.github.com>
2025-10-30 19:08:03 +0300

    IOrderService & OrderProfile

---
+++ Core/Services.Abstraction/Contracts/IOrderSevice.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Dtos.OrderModule;
+
+namespace Services.Abstraction.Contracts
+{
+    public interface IOrderSevice
+    {
+        //GrtById ==> Take Guid id ==> return OrderResult
+        Task<OrderResult> GetOrderByIdAsync(Guid id);
+        //GetAllByEmail ==> Take string Email ==> return IEnumerable<OrderResult>
+        Task<IEnumerable<OrderResult>> GetOrdersByEmailAsync(string userEmail);
+        //CreateOrder ==> Take OrderRequest , string Email ==> return OrderResult
+        Task<OrderResult> CreateOrderAsync(OrderRequest orderRequest, string userEmail);
+        //GetDeliveryMethods ==> return IEnumerable<DeliveryMethodResult>
+        Task<IEnumerable<DeliveryMethodResult>> GetDeliveryMethodsAsync();
+    }
+}
---
+++ Core/Services/MappingProfiles/OrderProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Domain.Entities.OrderModule;
+using Shared.Dtos.OrderModule;
+
+namespace Services.MappingProfiles
+{
+    internal class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Address , AddressDto>().ReverseMap();
+            CreateMap<DeliveryMethod , DeliveryMethodResult>();
+            CreateMap<OrderItem , OrderItemDto>()
+                .ForMember(dest => dest.ProductId, options => options.MapFrom(s => s.Product.ProductId))
+                .ForMember(dest => dest.ProductName, options => options.MapFrom(s => s.Product.ProductName))
+                .ForMember(dest => dest.PictureUrl, options => options.MapFrom(s => s.Product.PictureUrl));
+        
+            CreateMap<Order , OrderResult>()
+                .ForMember(dest => dest.PaymentStatus, options => options.MapFrom(s => s.PaymentStatus.ToString()))
+                .ForMember(dest => dest.DeliveryMethod, options => options.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(dest => dest.Total, options => options.MapFrom(s => s.SubTotal + s.DeliveryMethod.Price));
+
+
+        }
+    }
+}
