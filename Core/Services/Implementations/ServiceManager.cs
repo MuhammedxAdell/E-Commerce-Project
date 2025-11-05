cc3d856bdeef84fce55e4bc0fb704c5b5dd5b146
Mohamed Adel <111653519+MuhammedxAdell@users.noreply.github.com>
2025-11-05 20:26:22 +0200

    Payment Controller

--- Core/Services/Implementations/ServiceManager.cs
+++ Core/Services/Implementations/ServiceManager.cs
@@ -2,18 +2,20 @@
 using Domain.Contracts;
 using Domain.Entities.IdentityModule;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Services.Abstraction.Contracts;
 using Shared.Common;
 
 namespace Services.Implementations
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository, UserManager<User> _userManager, IOptions<JwtOption> _options) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository, UserManager<User> _userManager, IOptions<JwtOption> _options , IConfiguration _configuration) : IServiceManager
     {
         private readonly Lazy<IProductService> _productService = new Lazy<IProductService>(() => new ProductService(_unitOfWork , _mapper));
         private readonly Lazy<IBasketService> _basketService = new Lazy<IBasketService>(() => new BasketService(_basketRepository, _mapper));
         private readonly Lazy<IAuthenticationService> _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(_userManager , _options , _mapper));
         private readonly Lazy<IOrderSevice> _orderService = new Lazy<IOrderSevice>(() => new OrderService( _mapper ,  _basketRepository ,  _unitOfWork));
+        private readonly Lazy<IPaymentService> _paymentService = new Lazy<IPaymentService>(() => new PaymentService(_configuration , _basketRepository , _unitOfWork , _mapper));
         public IProductService ProductService => _productService.Value;
 
         public IBasketService BasketService => _basketService.Value;
@@ -21,5 +23,7 @@ namespace Services.Implementations
         public IAuthenticationService AuthenticatioService => _authenticationService.Value;
 
         public IOrderSevice OrderSevice => _orderService.Value;
+
+        public IPaymentService PaymentService => _paymentService.Value;
     }
 }
---
+++ Infrastructure/Presentation/Controllers/PaymentsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstraction.Contracts;
+using Shared.Dtos.BasketModule;
+
+namespace Presentation.Controllers
+{
+    public class PaymentsController(IServiceManager _serviceManager ) : ApiController
+    {
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            return Ok(await _serviceManager.PaymentService.CreateOrUpdatePaymentIntentAsync(basketId));
+        }
+    }
+}
